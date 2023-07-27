f63a1e3a65be43ebe31f3a0ca2a0639053ea9de6
prohilla74 <rohillastudy74@gmail.com>
2023-07-27 19:51:23 -0400

    added info

--- MemoeryBarrier/MemoeryBarrier.cpp
+++ MemoeryBarrier/MemoeryBarrier.cpp
@@ -2,6 +2,31 @@
 #include <thread>
 #include <iostream>
 
+/*
+In computer programming, a memory barrier, also known as a memory fence or memory synchronization barrier,
+is a mechanism used to enforce ordering constraints on memory operations performed by multiple threads
+or processors.It ensures that memory access operations(e.g., read and write) are properly synchronized 
+and do not violate the intended order of execution.
+
+In a multi - threaded environment, threads can access shared memory concurrently, which can lead to
+various issues like data races, where multiple threads try to accessand modify shared data simultaneously.
+Memory barriers help prevent these issues by establishing a clear order of execution for memory operations.
+
+There are two main types of memory barriers :
+
+Compiler Memory Barriers :
+These barriers are specific to the compilerand ensure that the compiler does not reorder memory operations around the barrier.For example, the volatile keyword in C++ is often used to create a compiler memory barrier.
+*/
+
+volatile int complier_sharedVariable;
+
+/* The 'volatile' keyword prevents compiler optimizations that could reorder reads / writes to sharedVariable
+Hardware Memory Barriers :
+These barriers involve CPU instructions that enforce ordering of memory operations at the hardware level.They ensure that memory operations on one processor core become visible to other cores in the correct order.Different processors have different instructions for memory barriers.For instance, on x86, the "MFENCE" instruction serves as a full memory barrier.
+
+Here's a simple example to illustrate how a memory barrier can be used:
+*/
+
 std::atomic<int> sharedVariable = 0;
 
 void writerThread() {
